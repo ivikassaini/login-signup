288c5517f6c52cb6a9ce440e2a9dad8d299f1030
Vikas <vickydelhi03@gmail.com>
2025-01-10 14:26:38 +0530

    initial commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { handleSuccess } from '../utils';
+import { ToastContainer } from 'react-toastify';
+
+function Home() {
+  const[loggedInUser, setLoggedInUser] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoggedInUser(localStorage.getItem('loggedInUser'))
+  },[])
+
+  const handleLogout = (e) => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('loggedInUser');
+    handleSuccess('Logged out successfully');
+    setTimeout(() =>{
+      navigate('/login');
+    }, 1000);
+  }
+
+  return (
+    <div>
+    <h1>Welcome {loggedInUser}</h1>
+    <button onClick={handleLogout}>Logout</button>
+    <ToastContainer />
+    </div>
+  )
+}
+
+export default Home
